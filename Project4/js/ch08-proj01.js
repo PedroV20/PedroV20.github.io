618781e786f9fb5318cde59a9682ce712f96ba6b
Pedro Victoriano <pedro@here.com>
2023-11-03 09:01:40 -0400

    added js files

--- Project4/js/ch08-proj01.js
+++ Project4/js/ch08-proj01.js
@@ -1,12 +1,17 @@
+const tax_rate = prompt('Enter tax rate (0.10)');
+const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 for( let c of cart){
    outputClassRow(c)
 }
 function outputClassRow(thisData){
    document.write('<tr>')
-   document.write('<td>' + '<img src="images/'  + thisData.product.filename + '">' + '</td>' )
+   document.write('<td>' + '<img src="images/'  + thisData.product.filename + '"' + '</td>' )
    document.write('<td>' + thisData.product.title + '</td>')
    document.write('<td>' + thisData.quantity + '</td>')
    document.write('<td>' + thisData.product.price + '</td>')
-   document.write(calculateTotal)
+   document.write('<td>' + calculateTotal(thisData.product.price, thisData.quantity) + '</td>')
+   document.write('</tr>')
 }
+
+
--- Project4/js/functions.js
+++ Project4/js/functions.js
@@ -1,4 +1,50 @@
 /* define your functions here */
-q = quanity;
-p = price;
-calculateTotal(q * p);
+// <script type ="text/JavaScript" scr="js/data.js"></script>
+/*
+q = quantity;
+c = price;
+p = title;
+calculateSub(3 * calculateTotal)
+calculateTotal(q * c);
+ for( let c of cart){
+    outputClassRow(c)
+ }
+
+*/
+
+function calculateTotal(p, q) {
+    return p * q;
+}
+
+function calculateSubTotal(cart){
+    let SubTotal = 0;
+    for (const item of cart){
+    const price = item.product.price; 
+    const quantity = item.quantity;
+    const itemSubTotal = price * quantity;
+    SubTotal = itemSubTotal + SubTotal;
+    }
+    return SubTotal;
+}
+function calculateTax(tax_rate){
+   const SubTotal = calculateSubTotal(cart);
+    return SubTotal * tax_rate;
+}
+function calculateShipping(shipping_threshold){
+    const SubTotal = calculateSubTotal(cart);
+
+    if (SubTotal < shipping_threshold) {
+        return 40;
+    }
+    else {
+        return 0;
+    }
+}
+function calculateGrandTotal(){
+
+    const SubTotal = calculateSubTotal(cart);
+    const Tax = calculateTax(tax_rate);
+    const Shipping = calculateShipping(shipping_threshold);
+
+    return SubTotal + Tax + Shipping;
+}
